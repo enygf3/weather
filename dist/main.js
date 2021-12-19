1209c1ac1d2dc059edb1f964012b9b47e148c3ae
enygf <gg.wp.0004.18@gmail.com>
2021-12-19 04:51:28 +0300

    test

---
+++ dist/main.js
@@ -0,0 +1,32 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ (() => {
+
+eval("throw new Error(\"Module build failed (from ./node_modules/babel-loader/lib/index.js):\\nSyntaxError: /Users/enygf/Documents/apps/innowise/weather/src/index.js: Unexpected reserved word 'await'. (6:19)\\n\\n\\u001b[0m \\u001b[90m 4 |\\u001b[39m\\u001b[0m\\n\\u001b[0m \\u001b[90m 5 |\\u001b[39m btn\\u001b[33m.\\u001b[39maddEventListener(\\u001b[32m\\\"click\\\"\\u001b[39m\\u001b[33m,\\u001b[39m () \\u001b[33m=>\\u001b[39m {\\u001b[0m\\n\\u001b[0m\\u001b[31m\\u001b[1m>\\u001b[22m\\u001b[39m\\u001b[90m 6 |\\u001b[39m     \\u001b[36mlet\\u001b[39m response \\u001b[33m=\\u001b[39m \\u001b[36mawait\\u001b[39m fetch(\\u001b[32m\\\"http://api.weatherapi.com/v1/current.json?key=f3355337615d498fbfa03825211912&q=Minsk\\\"\\u001b[39m)\\u001b[33m;\\u001b[39m\\u001b[0m\\n\\u001b[0m \\u001b[90m   |\\u001b[39m                    \\u001b[31m\\u001b[1m^\\u001b[22m\\u001b[39m\\u001b[0m\\n\\u001b[0m \\u001b[90m 7 |\\u001b[39m     \\u001b[36mlet\\u001b[39m commits \\u001b[33m=\\u001b[39m \\u001b[36mawait\\u001b[39m response\\u001b[33m.\\u001b[39mjson()\\u001b[33m;\\u001b[39m\\u001b[0m\\n\\u001b[0m \\u001b[90m 8 |\\u001b[39m\\u001b[0m\\n\\u001b[0m \\u001b[90m 9 |\\u001b[39m     console\\u001b[33m.\\u001b[39mlog(commits)\\u001b[0m\\n    at Parser._raise (/Users/enygf/Documents/apps/innowise/weather/node_modules/@babel/parser/lib/index.js:569:17)\\n    at Parser.raiseWithData (/Users/enygf/Documents/apps/innowise/weather/node_modules/@babel/parser/lib/index.js:562:17)\\n    at Parser.raise (/Users/enygf/Documents/apps/innowise/weather/node_modules/@babel/parser/lib/index.js:523:17)\\n    at Parser.checkReservedWord (/Users/enygf/Documents/apps/innowise/weather/node_modules/@babel/parser/lib/index.js:13047:12)\\n    at Parser.parseIdentifierName (/Users/enygf/Documents/apps/innowise/weather/node_modules/@babel/parser/lib/index.js:13001:12)\\n    at Parser.parseIdentifier (/Users/enygf/Documents/apps/innowise/weather/node_modules/@babel/parser/lib/index.js:12971:23)\\n    at Parser.parseExprAtom (/Users/enygf/Documents/apps/innowise/weather/node_modules/@babel/parser/lib/index.js:12070:27)\\n    at Parser.parseExprSubscripts (/Users/enygf/Documents/apps/innowise/weather/node_modules/@babel/parser/lib/index.js:11654:23)\\n    at Parser.parseUpdate (/Users/enygf/Documents/apps/innowise/weather/node_modules/@babel/parser/lib/index.js:11634:21)\\n    at Parser.parseMaybeUnary (/Users/enygf/Documents/apps/innowise/weather/node_modules/@babel/parser/lib/index.js:11609:23)\");\n\n//# sourceURL=webpack://weather/./src/index.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module doesn't tell about it's top-level declarations so it can't be inlined
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/index.js"]();
+/******/ 	
+/******/ })()
+;
