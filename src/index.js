dd59c3d5ae99c43d2ead3044ab6a99724e31a2c7
Artem Kondzirovski <78654438+enygf3@users.noreply.github.com>
2022-02-07 17:16:45 +0300

    added user input, changed design

--- src/index.js
+++ src/index.js
@@ -7,37 +7,52 @@ const app = document.getElementById("app")
 
 let obj;
 
-fetch(`${_apiBase}current.json?key=${_apiKey}&q=London&aqi=no`)
+let input = document.querySelector(".block-input")
+let cityValue = "";
+input.addEventListener("keydown", (e) => {
+    if(e.key == "Enter") {
+        cityValue = input.value
+        console.log(cityValue)
+        if(cityValue != "" && cityValue != " ") {
+            fetch(`${_apiBase}current.json?key=${_apiKey}&q=${cityValue}&aqi=no`)
+            .then(res => res.json())
+            .then(result => valid(result))
+        }
+    }
+})
+
+fetch(`${_apiBase}current.json?key=${_apiKey}&q=Minsk&aqi=no`)
     .then(res => res.json())
     .then(result => valid(result))
 
 function valid(res) {
     obj = res;
 
-    let city = document.querySelector(".block-city")
-    city.innerHTML = obj.location.name;
-
-    let temp = document.querySelector(".block-temp")
-    temp.innerHTML = "Temperature: " + obj.current.temp_c + " C";
-
-    let tempFeels = document.querySelector(".block-temp-feels")
-    tempFeels.innerHTML = "Feels like: " + Math.round(obj.current.feelslike_c) + " C";
-
-    let windDir = document.querySelector(".block-wind-dir")
-    windDir.innerHTML = "Wind direction: " + obj.current.wind_dir;
-
-    let windSpeed = document.querySelector(".block-wind-speed")
-    windSpeed.innerHTML = "Wind speed: " + Math.round(obj.current.wind_kph) + " km/h";
-
-    let humidity = document.querySelector(".block-humidity")
-    humidity.innerHTML = "Humidity: " + Math.round(obj.current.humidity) + "%";
-
-    let updated = document.querySelector(".block-updated")
-    updated.innerHTML = "Last updated: " + obj.current.last_updated;
-
-    if(obj.current.is_day == 1) {
-        app.style.background = "linear-gradient(45deg, rgba(20,3,61,1) 36%, rgba(150,152,34,1) 100%)"
+    if(obj) {
+        let city = document.querySelector(".app-city")
+        city.innerHTML = obj.location.name;
+    
+        let temp = document.querySelector(".app-temp")
+        temp.innerHTML = Math.round(obj.current.temp_c) + "°";
+    
+        let cloud = document.querySelector(".app-cloud")
+        cloud.innerHTML = obj.current.condition.text;
+    
+        let windDir = document.querySelector(".block-wind-dir")
+        windDir.innerHTML = "Wind direction: " + obj.current.wind_dir;
+    
+        let windSpeed = document.querySelector(".block-wind-speed")
+        windSpeed.innerHTML = Math.round(obj.current.wind_kph) + " km/h";
+    
+        let humidity = document.querySelector(".block-humidity")
+        humidity.innerHTML = "Humidity: " + Math.round(obj.current.humidity) + "%";
+    
+        if(obj.current.is_day == 1) {
+            app.style.background = "linear-gradient(45deg, rgba(20,3,61,1) 36%, rgba(150,152,34,1) 100%)"
+        } else {
+            app.style.background = "#14033D"
+        }
     } else {
-        app.style.background = "#14033D"
+        alert("Some error has occured. Try other city or check your internet connection")
     }
 }
