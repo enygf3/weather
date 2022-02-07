0620c34ea6685959864f78fa95a539c25e51336e
Artem Kondzirovski <78654438+enygf3@users.noreply.github.com>
2022-02-07 17:21:22 +0300

    changed http query to https

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import "./styles/main.sass"
 
-const _apiBase = "http://api.weatherapi.com/v1/"
+const _apiBase = "https://api.weatherapi.com/v1/"
 const _apiKey = "f3355337615d498fbfa03825211912"
 
 const app = document.getElementById("app")
