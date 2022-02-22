8359ec89acb5d239508269af63fc7773e533a110
Artem Kondzirovski <78654438+enygf3@users.noreply.github.com>
2022-02-23 01:54:25 +0300

    deleted unused event listener

--- src/index.js
+++ src/index.js
@@ -64,7 +64,3 @@ function valid(res) {
         alert("Some error has occured. Try other city or check your internet connection")
     }
 }
-
-input.addEventListener("click", (e) => {
-    windDir.innerHTML = window.innerHeight
-})
